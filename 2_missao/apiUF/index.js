a8557d87de18c6d4b212101ad2acf71ccfcf5f67
athena272 <guilhermera272@gmail.com>
2023-08-13 13:32:39 -0300

    Update index.js

--- 2_missao/apiUF/index.js
+++ 2_missao/apiUF/index.js
@@ -7,6 +7,27 @@ app.get('/ufs', (req, res) => {
     res.json(colecaoUf)
 })
 
+app.get('/ufs/:id', (req, res) => {
+    const idUF = parseInt(req.params.id)
+    let uf
+    let errorMessage = 'Invalid request'
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.find(uf => uf.id === idUF)
+
+        if (!uf) {
+            errorMessage = 'UF not found'
+        }
+    }
+
+    if (uf) {
+        res.json(uf)
+    } else {
+        res.status(404).send({ 'error': errorMessage })
+    }
+})
+
+
 app.listen(8080, () => {
     console.log('🔥 Server started at port 8080 🔥')
 })
